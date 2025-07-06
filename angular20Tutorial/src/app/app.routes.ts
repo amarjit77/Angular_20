c8e6e218d73b76946c34f6eed40f912205841e1d
AMAR V. KAMBLE <kamarjit77@gmail.com>
2025-07-06 18:41:54 +0530

    Add code for navigation, bootstrap and register bootstrap npm entry and routerlink configration

--- angular20Tutorial/src/app/app.routes.ts
+++ angular20Tutorial/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { Databinding } from './components/databinding/databinding';
+import { SignalEx } from './components/signal-ex/signal-ex';
+import { Controlflow } from './components/controlflow/controlflow';
+import { User } from './components/user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Admin', component: Admin },
+    { path: 'User', component: User },
+    { path: 'ControlFlow', component: Controlflow },
+    { path: 'Signal', component: SignalEx },
+    { path: 'Databinding', component: Databinding },
+];
