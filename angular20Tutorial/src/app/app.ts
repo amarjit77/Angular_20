4a0f7c9974d9f42e77171e7e9cfca61b98bf0674
AMAR V. KAMBLE <kamarjit77@gmail.com>
2025-07-06 15:04:19 +0530

    Add code of data binding

--- angular20Tutorial/src/app/app.ts
+++ angular20Tutorial/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Admin } from "./components/admin/admin";
 import { User } from "./components/user/user";
+import { Databinding } from "./components/databinding/databinding";
 
 @Component({
   selector: 'app-root',
-  imports: [Admin, User],
+  imports: [Admin, User, Databinding],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
